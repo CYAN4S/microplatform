925ba3bf88f4ba8480b10190f6dcd072bd0e7e85
CYAN4S <37798132+CYAN4S@users.noreply.github.com>
2020-12-20 01:28:57 +0900

    Fix button, Add quit system

---
+++ Assets/Scripts/GameQuit.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameQuit : MonoBehaviour
+{
+    [SerializeField] private GameObject messageAndroid;
+    [SerializeField] private GameObject messageWindows;
+
+    private GameObject _quitMessage;
+    private float _firstQuitTime = -3f;
+    
+    private void Awake()
+    {
+        _quitMessage = Application.platform == RuntimePlatform.Android ? messageAndroid : messageWindows;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (_firstQuitTime + 3f >= Time.time)
+            {
+                Application.Quit();
+                return;
+            }
+            
+            _quitMessage.SetActive(true);
+            StartCoroutine(nameof(HideMessage));
+            _firstQuitTime = Time.time;
+        }
+    }
+
+    private IEnumerator HideMessage()
+    {
+        yield return new WaitForSeconds(3f);
+        _quitMessage.SetActive(false);
+    }
+}
